107fba2f2bffc8911321798468fcd82990b9483c
Jay Nguyen <jnguyen0291@gmail.com>
2022-03-15 15:02:38 -0700

    React App & useState hook practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// YOU CAN ONLY USE HOOKS INSIDE OF FUNCTION COMPONENTS; CANNOT USE THEM IN CLASS COMPONENTS. WHEN YOU UPDATE THE STATE, THE COMPONENT RE-RENDERS.
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// useState TAKES TWO VALUES: A DEFAULT STATE AND THE UPDATE/SET FUNCTION. count IS THE DEFAULT STATE; setCount IS A FUNCTION THAT UPDATES THE STATE.
+	const [count, setCount] = useState(0);
+
+	function decrementCount() {
+		// THERE IS A SECOND VERSION OF THE SET FUNCTION WHICH ALLOWS YOU TO PASS IN A FUNCTION THAT TAKES IN THE PREVIOUS STATE VALUE; IN THIS CASE, THE PREVIOUS COUNT.
+		setCount((prevCount) => prevCount - 1);
+	}
+
+	function incrementCount() {
+		// THERE IS A SECOND VERSION OF THE SET FUNCTION WHICH ALLOWS YOU TO PASS IN A FUNCTION THAT TAKES IN THE PREVIOUS STATE VALUE; IN THIS CASE, THE PREVIOUS COUNT.
+		setCount((prevCount) => prevCount + 1);
+	}
+
+	return (
+		<>
+			<button onClick={decrementCount}>-</button>
+			<span>{count}</span>
+			<button onClick={incrementCount}>+</button>
+		</>
+	);
 }
 
 export default App;
